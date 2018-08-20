b78a04963d4290127742e62e6631baee9a7fafc3
Yuta Omori <lostforest0@gmail.com>
2018-08-20 08:25:59 +0000

    precompile

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,6 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
-//= require jquery/dist/jquery.js
-//= require bootstrap/dist/js/bootstrap.min
+//= require node_modules/jquery/dist/jquery.js
+//= require node_modules/bootstrap/dist/js/bootstrap.min
 //= require_tree .
